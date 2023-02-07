742013a0d89de66c39bc8d169dd86b50bd5a131d
Salazaku III <zaku.dev@gmail.com>
2023-02-07 17:24:38 -0500

    I setup the wireframe, routing and views logic.

--- userAuth/urls.py
+++ userAuth/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatters = [
-    path('', views.home, name="index"),
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('ua/signin', views.sign_in, name="signin"),
+    path('ua/regis', views.register, name="register"),
+    path('ua/logout', views.logout, name="logout")
 ]
--- userAuth/views.py
+++ userAuth/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'userAuth/main.html')
+
+def sign_in(request):
+
+    if request.method=="POST":
+        pass
+
+    return render(request, 'userAuth/signin.html')
+
+def register(request):
+
+    if request.method=="POST":
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email_address']
+        username = request.POST['uname']
+        pwd = request.POST['pd']
+        conf_pwd = request.POST['conf_pd']
+
+        print("IT WORKS!")
+
+    return render(request, 'userAuth/register.html')
+
+def logout(request):
+    pass
